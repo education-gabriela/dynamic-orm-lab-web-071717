7a0b8727c0b6d714aafc4e60a4ad49da095c39b6
gabidavila <gabidavila@gmail.com>
2017-07-26 22:05:47 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,11 @@
 require_relative "../config/environment.rb"
-require 'active_support/inflector'
-require 'interactive_record.rb'
+require_relative "../lib/interactive_record.rb"
+require "active_support/inflector"
+
 
 class Student < InteractiveRecord
 
+  self.column_names.each do |field|
+    attr_accessor field.to_sym
+  end
 end
